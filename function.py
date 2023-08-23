90e27cedc5e699baac0aba297f30799ed3a8cfb3
ADITYA <aditya11surve08@gmail.com>
2023-08-23 21:34:37 +0530

    implemented the add button

--- function.py
+++ function.py
@@ -1,7 +1,7 @@
 FILEPATH = "todos.txt"
 
 def get_todos(filepath=FILEPATH):
-    with open(filepath, "r") as file_local:
+    with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
