e5776653d753a30bf31b91265c7b071d4edcd127
ADITYA <aditya11surve08@gmail.com>
2023-08-24 19:59:03 +0530

    implemented the edit button

--- cli.py
+++ cli.py
@@ -11,15 +11,15 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = function.get_todos(filepath='todos.txt')
+        todos = function.get_todos()
 
         todos.append(todo + "\n" )
 
-        function.write_todos(filepath="todos.txt", todos_arg=todos)
+        function.write_todos(todos)
 
     elif user_action.startswith("show"):
 
-        todos = function.get_todos(filepath='todos.txt')
+        todos = function.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip("\n")
@@ -33,12 +33,12 @@ while True:
 
             number = number - 1
 
-            todos = function.get_todos(filepath='todos.txt')
+            todos = function.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n"
 
-            function.write_todos(filepath="todos.txt", todos_arg=todos)
+            function.write_todos(todos)
         except ValueError:
             print("Your command is invalid.")
             continue
@@ -48,13 +48,13 @@ while True:
 
             number = int(input(user_action[11:]))
 
-            todos = function.get_todos(filepath='todos.txt')
+            todos = function.get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip("\n")
             todos.pop(index)
 
-            function.write_todos(filepath="todos.txt", todos_arg=todos)
+            function.write_todos(todos)
 
             message = f"todo {todo_to_remove} was removed from the list."
             print(message)
